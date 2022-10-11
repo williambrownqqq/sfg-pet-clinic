62c769e52de3e72747db086b375c1d5864f0a6a7
Alex <iamalexzanchenko@gmail.com>
2022-10-11 15:17:00 +0200

    Added Specialties Map Impl, closes #31

---
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/com/zanchenko/alexey/sfgclinic/services/SpecialtiesService.java
@@ -0,0 +1,6 @@
+package com.zanchenko.alexey.sfgclinic.services;
+
+import com.zanchenko.alexey.sfgclinic.model.Speciality;
+
+public interface SpecialtiesService extends CrudService<Speciality, Long>{
+}
