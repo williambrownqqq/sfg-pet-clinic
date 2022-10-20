3091937a8f9d850f62b48da90df9f332fd9060f6
Alex <iamalexzanchenko@gmail.com>
2022-10-20 20:23:51 +0200

    Udated base entity with JPA annotations, we add @MappedSuperclass to BaseEntity, closes #35 If we run Spring Boot, I expect to a toss an exception. We do need to complete the mapping for the other classes in our model. In the JPA, we have a number of annotations to do.

--- sfg-pet-clinic/pet-clinic-data/src/main/java/com/zanchenko/alexey/sfgclinic/model/BaseEntity.java
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/com/zanchenko/alexey/sfgclinic/model/BaseEntity.java
@@ -1,8 +1,19 @@
 package com.zanchenko.alexey.sfgclinic.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
-
+@MappedSuperclass // this established this ass a base class to JPA. // What we are
+//doing is saying, "Hey JPA, we're going to inherit from this class or other classes
+//        are going to be inheriting it. We don't need this specific class mapped to the
+//database" // Нам не нужно, чтобы этот конкретный класс отображался на база данных
 public class BaseEntity implements Serializable {
+    @Id // This annotation tells JPA that this is the ID value.
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // Now IDENTITY, which is what we've selected is saying that we're
+    //going to insert it in the database is going to provide us the identity value.
+    // Теперь ИДЕНТИЧНОСТЬ, которую мы выбрали, говорит, что мы собирается вставить его в базу данных, чтобы предоставить нам значение идентификатора.
     private Long id;
 
     public Long getId() {
@@ -13,3 +24,31 @@ public class BaseEntity implements Serializable {
         this.id = id;
     }
 }
+
+
+
+// GenerationType
+//AUTO can give you some problems as far
+//as unpredictable results. IDENTITY does tie you to the specific database.
+//SEQUENCE is a good choice.
+// AUTO может доставить вам некоторые проблемы как непредсказуемые результаты. IDENTITY привязывает вас к конкретной базе данных. ПОСЛЕДОВАТЕЛЬНОСТЬ - хороший выбор.
+
+
+// TABLE means that we're going to have a database table with the sequence number
+//on that, so we'll have a value on a database table that we're going to use to
+//determine the maximum value and then the next value. A SEQUENCE, databases have
+//what's called a sequence generator, where you can say give me the next number in a
+//sequence. The functionality there and this is depends on the underlying
+//database. Now IDENTITY, which is what we've selected is saying that we're
+//going to insert it in the database is going to provide us the identity value.
+
+// TABLE означает, что у нас будет таблица базы данных с порядковым номером на этом, поэтому у нас будет значение в
+// таблице базы данных, которое мы собираемся использовать для определить максимальное значение, а затем следующее значение.
+// ПОСЛЕДОВАТЕЛЬНОСТЬ, базы данных то, что называется генератором последовательности, где вы можете сказать мне следующее число
+// в последовательность. Функциональность там, и это зависит от основного база данных.
+// Теперь ИДЕНТИЧНОСТЬ, которую мы выбрали, говорит, что мы собирается вставить его в базу данных, чтобы предоставить нам значение идентификатора.
+// AUTO is basically saying that we're
+//gonna pick one and we're going to allow Hibernate to look at the underlying
+//database and determine a strategy to use.
+// AUTO в основном говорит, что мы собираемся выбрать один, и мы позволим Hibernate взглянуть на основной базы данных
+// и определить стратегию для использования.
