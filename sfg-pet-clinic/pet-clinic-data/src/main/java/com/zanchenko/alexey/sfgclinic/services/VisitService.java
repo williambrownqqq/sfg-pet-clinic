ed44487915038b8d3205c313e225dfae2b6fea3b
Alex <iamalexzanchenko@gmail.com>
2022-10-26 12:27:05 +0200

    IMplemented Map impl for Visit Service, closes #46

---
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/com/zanchenko/alexey/sfgclinic/services/VisitService.java
@@ -0,0 +1,6 @@
+package com.zanchenko.alexey.sfgclinic.services;
+
+import com.zanchenko.alexey.sfgclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long>{
+}
---
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/com/zanchenko/alexey/sfgclinic/services/map/VisitMapService.java
@@ -0,0 +1,37 @@
+package com.zanchenko.alexey.sfgclinic.services.map;
+
+import com.zanchenko.alexey.sfgclinic.model.Visit;
+import com.zanchenko.alexey.sfgclinic.services.VisitService;
+
+import java.util.Set;
+
+public class VisitMapService extends AbstractMapService<Visit, Long> implements VisitService {
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Visit visit) {
+       super.delete(visit);
+    }
+
+    @Override
+    public Visit save(Visit visit) {
+        if(visit.getPet() == null || visit.getPet() == null
+                || visit.getPet().getId() == null || visit.getPet().getOwner().getId() == null){
+            throw new RuntimeException("Invalid Visit");
+        }
+        return super.save(visit);
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+}
