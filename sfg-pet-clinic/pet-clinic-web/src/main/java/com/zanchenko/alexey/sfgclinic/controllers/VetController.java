3fff47f40357bb306b26fe28dc2e8d4a72fab9f4
williambrownqqq <alexey.zanchenko@gmail.com>
2023-02-09 13:32:26 +0200

    added json. endpoint closes #69

--- sfg-pet-clinic/pet-clinic-web/src/main/java/com/zanchenko/alexey/sfgclinic/controllers/VetController.java
+++ sfg-pet-clinic/pet-clinic-web/src/main/java/com/zanchenko/alexey/sfgclinic/controllers/VetController.java
@@ -1,9 +1,14 @@
 package com.zanchenko.alexey.sfgclinic.controllers;
 
+import com.zanchenko.alexey.sfgclinic.model.Vet;
 import com.zanchenko.alexey.sfgclinic.services.VetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Set;
 
 @Controller
 public class VetController {
@@ -18,4 +23,14 @@ public class VetController {
         model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
+
+    @GetMapping("/api/vets") // I'm using GetMapping to limit this to get requests
+    public @ResponseBody Set<Vet> getVetsJson(){ //  I'm mapping it to the
+        // URL of api/vets. ResponseBody is going to say I want this to be
+        // formatted as JSON depending on the request type.
+        // It's going to fall to to
+        //JSON but I could also set up Spring to render XML as well.
+        // But here we're going to default out to JSON
+        return vetService.findAll();
+    } //
 }
